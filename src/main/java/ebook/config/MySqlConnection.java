7c5ea7a4b1fe5947180bf4148b1142e6114103cc
minhhuy243 <minhhuy243@gmail.com>
2021-07-13 00:22:29 +0700

    test

--- src/main/java/ebook/config/MySqlConnection.java
+++ src/main/java/ebook/config/MySqlConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class MySqlConnection {
 	/* database connection information */
-	private static final String url = "jdbc:mysql://localhost:3306/ebook";
-	private static final String username = "root";
-	private static final String password = "";
+	private static final String url = "jdbc:mysql://us-cdbr-east-04.cleardb.com/heroku_79698f772917849";
+	private static final String username = "b6afcb701a72c4";
+	private static final String password = "c1707a61";
 	
 	public static Connection getConnection() {
 		try {
