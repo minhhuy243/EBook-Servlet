33aa9e134a7f536e5ef898cbb5ab5805d47d42fd
minhhuy243 <minhhuy243@gmail.com>
2021-07-13 00:50:44 +0700

    test

--- src/main/java/ebook/config/MySqlConnection.java
+++ src/main/java/ebook/config/MySqlConnection.java
@@ -8,22 +8,28 @@ import java.sql.SQLException;
 
 public class MySqlConnection {
 	/* database connection information */
-	private static final String url = "jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_79698f772917849";
-	private static final String username = "b6afcb701a72c4";
-	private static final String password = "c1707a61";
+//	private static final String url = "jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_79698f772917849";
+//	private static final String username = "b6afcb701a72c4";
+//	private static final String password = "c1707a61";
 	
 	public static Connection getConnection() {
 		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			return DriverManager.getConnection(url, username, password);
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
 
 			String username = jdbUri.getUserInfo().split(":")[0];
 			String password = jdbUri.getUserInfo().split(":")[1];
 			String port = String.valueOf(jdbUri.getPort());
 			String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-		} catch (URISyntaxException e) {
+			return DriverManager.getConnection(jdbUrl, username, password);
+		} catch (ClassNotFoundException e) {
+			// TODO:
 			e.printStackTrace();
+			System.out.println("Driver could not be found.");
+		} catch (SQLException | URISyntaxException e) {
+			// TODO:
+			e.printStackTrace();
+			System.out.println("Can not connect to database.");
 		}
 		return null;
 	}
